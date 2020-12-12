fa300d3bba38be0c66ad095b2268b9cfd5b2851a
Minh <lenhatminhchodien@gmail.com>
2020-12-12 16:50:49 +0700

    them routes

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Category Product
+Route::get('/add-category-product','CategoryProduct@add_category_product');
+Route::get('/edit-category-product/{category_product_id}','CategoryProduct@edit_category_product');
+Route::get('/delete-category-product/{category_product_id}','CategoryProduct@delete_category_product');
+Route::get('/all-category-product','CategoryProduct@all_category_product');
+
+Route::get('/unactive-category-product/{category_product_id}','CategoryProduct@unactive_category_product');
+Route::get('/active-category-product/{category_product_id}','CategoryProduct@active_category_product');
+
+
+Route::post('/save-category-product','CategoryProduct@save_category_product');
+Route::post('/update-category-product/{category_product_id}','CategoryProduct@update_category_product');
+
+//Brand Product
+Route::get('/add-brand-product','BrandProduct@add_brand_product');
+Route::get('/edit-brand-product/{brand_product_id}','BrandProduct@edit_brand_product');
+Route::get('/delete-brand-product/{brand_product_id}','BrandProduct@delete_brand_product');
+Route::get('/all-brand-product','BrandProduct@all_brand_product');
+
+Route::get('/unactive-brand-product/{brand_product_id}','BrandProduct@unactive_brand_product');
+Route::get('/active-brand-product/{brand_product_id}','BrandProduct@active_brand_product');
+
+Route::post('/save-brand-product','BrandProduct@save_brand_product');
+Route::post('/update-brand-product/{brand_product_id}','BrandProduct@update_brand_product');
+
+
