22234958ff1de5b18b3628e896d3c9bccfd0555e
unknown <Hoanghuy10061999@gmail.com>
2020-12-12 16:54:05 +0700

    da them routes

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Frontend 
+Route::get('/','HomeController@index' );
+Route::get('/trang-chu','HomeController@index');
+Route::post('/tim-kiem','HomeController@search');
+
+//Danh muc san pham trang chu
+Route::get('/danh-muc-san-pham/{slug_category_product}','CategoryProduct@show_category_home');
+Route::get('/thuong-hieu-san-pham/{brand_slug}','BrandProduct@show_brand_home');
+Route::get('/chi-tiet-san-pham/{product_slug}','ProductController@details_product');
+
+
+//Cart
+Route::post('/update-cart-quantity','CartController@update_cart_quantity');
+Route::post('/save-cart','CartController@save_cart');
+Route::get('/show-cart','CartController@show_cart');
+Route::get('/delete-to-cart/{rowId}','CartController@delete_to_cart');
+
+//Checkout
+
+Route::get('/login-checkout','CheckoutController@login_checkout');
+Route::get('/logout-checkout','CheckoutController@logout_checkout');
+Route::post('/add-customer','CheckoutController@add_customer');
+Route::post('/order-place','CheckoutController@order_place');
+Route::post('/login-customer','CheckoutController@login_customer');
+Route::get('/checkout','CheckoutController@checkout');
+Route::get('/payment','CheckoutController@payment');
+Route::post('/save-checkout-customer','CheckoutController@save_checkout_customer');
+
+//Order
+Route::get('/manage-order','CheckoutController@manage_order');
+Route::get('/view-order/{orderId}','CheckoutController@view_order');
+
+
