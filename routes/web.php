36715c699266a1a284aefe5f2d0dbfc8122cddab
duc le <ducdiduc761311@gmail.com>
2020-12-12 16:53:41 +0700

    da them routes thanh cong

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Backend
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_dashboard');
+Route::get('/logout','AdminController@logout');
+Route::post('/admin-dashboard','AdminController@dashboard');
+
+
+//Product
+Route::get('/add-product','ProductController@add_product');
+Route::get('/edit-product/{product_id}','ProductController@edit_product');
+Route::get('/delete-product/{product_id}','ProductController@delete_product');
+Route::get('/all-product','ProductController@all_product');
+
+Route::get('/unactive-product/{product_id}','ProductController@unactive_product');
+Route::get('/active-product/{product_id}','ProductController@active_product');
+
+Route::post('/save-product','ProductController@save_product');
+Route::post('/update-product/{product_id}','ProductController@update_product');
+
+
+
